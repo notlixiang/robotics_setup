f2a69471c7f6a7e055fee8b87d6ed80feb197b16
NULL <lixiang3.1416@qq.com>
2018-11-08 18:19:20 +0800

    edit ros.sh

--- ros.sh
+++ ros.sh
@@ -26,54 +26,32 @@ set -x
 # only install
 if [ "$DISTRIB_RELEASE" = "16.04" ]; then
     # source: http://wiki.ros.org/kinetic/Installation/Ubuntu
-    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+    sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.ustc.edu.cn/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
     sudo apt-get update
     sudo apt-get install ros-kinetic-desktop-full
-    sudo apt-get install python-rosinstall
-
-    sudo apt-get install python-catkin-tools libgflags-dev
-
-    # If using bash:
-    #
-    # echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-    # source ~/.bashrc
-    #
-    #
-
-    # Add ros setup to zshrc if it isn't already present
-    LINE="source /opt/ros/kinetic/setup.zsh"
-    FILE=~/.zshrc
-    grep -q "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-fi
-
-if [ "$DISTRIB_RELEASE" = "14.04" ]; then
-    # source: http://wiki.ros.org/kinetic/Installation/Ubuntu
-    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-    sudo apt-get update
-    sudo apt-get install ros-indigo-desktop-full
-    sudo apt-get install python-rosinstall
-    if [ ! -d /etc/ros/rosdep/ ]
-    then
-        sudo rosdep init
-    fi
+    
+    sudo rosdep init
     rosdep update
-    sudo apt-get install python-catkin-tools libgflags-dev
+    # sudo apt-get install python-rosinstall
 
+    # sudo apt-get install python-catkin-tools libgflags-dev
 
     # If using bash:
     #
-    # echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-    # source ~/.bashrc
+    echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+    source ~/.bashrc
+    sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
     #
     #
 
     # Add ros setup to zshrc if it isn't already present
-    LINE="source /opt/ros/indigo/setup.zsh"
-    FILE=~/.zshrc
-    grep -q "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+    # LINE="source /opt/ros/kinetic/setup.zsh"
+    # FILE=~/.zshrc
+    # grep -q "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 fi
+
 
 
 cd $DIR
